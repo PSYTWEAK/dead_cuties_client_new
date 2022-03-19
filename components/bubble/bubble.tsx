7a0b20c66263e84fcf607047d240e716f3d75dd3
MagusVeles <0x9d0f6813fb1896ad8568d00c88a19350c5f8c57d@ethmail.cc>
2022-03-19 10:53:41 +0100

    added nullcheck for accessTokenContract

--- components/bubble/bubble.tsx
+++ components/bubble/bubble.tsx
@@ -7,130 +7,130 @@ import useDeadCutiesDepositAccessToken from "../../hooks/useDeadCutiesDepositAcc
 import useDeadCutiesMint from "../../hooks/useDeadCutiesMint";
 
 function Bubble({ setStartTimer, deadCutiesContract, accessTokenContract }) {
-  const inputRef = useRef(null);
-  const { account } = useWeb3React();
+    const inputRef = useRef(null);
+    const { account } = useWeb3React();
 
-  const [inputValue, setInputValue] = useState("1");
-  let [mintStep, setMintStep] = useState(0);
-  let [priceOfMint, setPriceOfMint] = useState(0);
+    const [inputValue, setInputValue] = useState("1");
+    let [mintStep, setMintStep] = useState(0);
+    let [priceOfMint, setPriceOfMint] = useState(0);
 
-  let { data } = useTokenBalance(account, "0xc5ec6B520d589f6375dCc237965DE9E2702476F3");
+    let { data } = useTokenBalance(account, accessTokenContract ? accessTokenContract.address : "");
 
-  let balance;
+    let balance;
 
-  useEffect(() => {
-    if (data) {
-      balance = data.toString();
-      if (balance === "1") {
-        setMintStep(1);
-      }
-    } else {
-      balance = "0";
-    }
-  }),
-    [data];
+    useEffect(() => {
+        if (data) {
+            balance = data.toString();
+            if (balance === "1") {
+                setMintStep(1);
+            }
+        } else {
+            balance = "0";
+        }
+    }),
+        [data];
 
-  const stepTrigger = function (pos) {
-    if (inputRef.current) {
-      if (pos === "up") {
-        inputRef.current.stepUp();
-      } else {
-        inputRef.current.stepDown();
-      }
-      setInputValue(inputRef.current.value);
-      let price = calculatePrice(inputRef.current.value);
-      setPriceOfMint(price);
-    }
-  };
+    const stepTrigger = function (pos) {
+        if (inputRef.current) {
+            if (pos === "up") {
+                inputRef.current.stepUp();
+            } else {
+                inputRef.current.stepDown();
+            }
+            setInputValue(inputRef.current.value);
+            let price = calculatePrice(inputRef.current.value);
+            setPriceOfMint(price);
+        }
+    };
 
-  const calculatePrice = (quantity) => {
-    let noOffer = quantity % 3;
-    let offer = Math.floor(quantity / 3);
-    let price = offer * 2 * 0.03 + noOffer * 0.03;
-    return price;
-  };
+    const calculatePrice = (quantity) => {
+        let noOffer = quantity % 3;
+        let offer = Math.floor(quantity / 3);
+        let price = offer * 2 * 0.03 + noOffer * 0.03;
+        return price;
+    };
 
-  const changeMintStep = async function () {
-    try {
-      if (mintStep === 1) {
-        // mintStep === 1 shows "Pay with access token" button and starts timer
-        await useDeadCutiesDepositAccessToken(deadCutiesContract);
-        setStartTimer(true);
-      }
-      if (mintStep === 2) {
-        // mintStep === 2 shows amount and reap souls button so you can put in here minting function
-        await useDeadCutiesMint(deadCutiesContract, Number(inputValue), priceOfMint);
-      }
-      setMintStep(mintStep + 1);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+    const changeMintStep = async function () {
+        try {
+            if (mintStep === 1) {
+                // mintStep === 1 shows "Pay with access token" button and starts timer
+                await useDeadCutiesDepositAccessToken(deadCutiesContract);
+                setStartTimer(true);
+            }
+            if (mintStep === 2) {
+                // mintStep === 2 shows amount and reap souls button so you can put in here minting function
+                await useDeadCutiesMint(deadCutiesContract, Number(inputValue), priceOfMint);
+            }
+            setMintStep(mintStep + 1);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-  const checkingInput = function (e) {
-    const leadingZero = e.target.value === "0" ? true : false;
-    setInputValue(leadingZero ? 1 : e.target.value);
-  };
+    const checkingInput = function (e) {
+        const leadingZero = e.target.value === "0" ? true : false;
+        setInputValue(leadingZero ? 1 : e.target.value);
+    };
 
-  const validation = function (e) {
-    let isNumber = false;
-    isNumber = e.key.match(/^[0-9]*$/) ? true : false;
+    const validation = function (e) {
+        let isNumber = false;
+        isNumber = e.key.match(/^[0-9]*$/) ? true : false;
 
-    const allowedKeys = ["ArrowUp", "ArrowDown", "Backspace", "Del"];
-    const isAllowedKey = allowedKeys.includes(e.key);
+        const allowedKeys = ["ArrowUp", "ArrowDown", "Backspace", "Del"];
+        const isAllowedKey = allowedKeys.includes(e.key);
 
-    if (isNumber === false && isAllowedKey === false) e.preventDefault();
-  };
+        if (isNumber === false && isAllowedKey === false) e.preventDefault();
+    };
 
-  return (
-    <div mint-step={mintStep} className="bubble">
-      <div className="bubble__text">
-        {mintStep === 0 && (
-          <>
-            <p>Your time isn't here yet</p>
-            <p>...your wallet needs an access token to continue</p>
-          </>
-        )}
-        {mintStep === 1 && (
-          <>
-            <p>Their time has come. They are wandering the Ether now. I can get these lost souls for you...</p>
-            <p>
-              ...as I have no use for them.
-              <br />
-              They are troubled by their death and aren't ready to move on beyond.
-            </p>
-            <p>For a fee, I will bind them to you until you are ready to release them.</p>
-          </>
-        )}
-        {mintStep === 2 && <>How many souls shall I bind to you?</>}
-      </div>
-      <div className="bubble__form">
-        {mintStep === 1 && (
-          <>
-            <button
-              onClick={() => {
-                changeMintStep();
-              }}
-            >
-              Pay the Reaper with your access token
-            </button>
-          </>
-        )}
-        {mintStep === 2 && (
-          <>
-            <span className="bubble__form-input">
-              <input ref={inputRef} type="number" min="1" onKeyPress={(e) => validation(e)} onChange={(e) => checkingInput(e)} value={inputValue} />
-              <span className="bubble__form-input-steps">
-                <button onClick={() => stepTrigger("up")}></button>
-                <button {...(inputValue === "1" ? { disabled: true } : {})} onClick={() => stepTrigger("down")}></button>
-              </span>
-            </span>
-            <button>reap cuties for the price of {priceOfMint} ETH</button>
-          </>
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div mint-step={mintStep} className="bubble">
+            <div className="bubble__text">
+                {mintStep === 0 && (
+                    <>
+                        <p>Your time isn't here yet</p>
+                        <p>...your wallet needs an access token to continue</p>
+                    </>
+                )}
+                {mintStep === 1 && (
+                    <>
+                        <p>Their time has come. They are wandering the Ether now. I can get these lost souls for you...</p>
+                        <p>
+                            ...as I have no use for them.
+                            <br />
+                            They are troubled by their death and aren't ready to move on beyond.
+                        </p>
+                        <p>For a fee, I will bind them to you until you are ready to release them.</p>
+                    </>
+                )}
+                {mintStep === 2 && <>How many souls shall I bind to you?</>}
+            </div>
+            <div className="bubble__form">
+                {mintStep === 1 && (
+                    <>
+                        <button
+                            onClick={() => {
+                                changeMintStep();
+                            }}
+                        >
+                            Pay the Reaper with your access token
+                        </button>
+                    </>
+                )}
+                {mintStep === 2 && (
+                    <>
+                        <span className="bubble__form-input">
+                            <input ref={inputRef} type="number" min="1" onKeyPress={(e) => validation(e)} onChange={(e) => checkingInput(e)} value={inputValue} />
+                            <span className="bubble__form-input-steps">
+                                <button onClick={() => stepTrigger("up")}></button>
+                                <button {...(inputValue === "1" ? { disabled: true } : {})} onClick={() => stepTrigger("down")}></button>
+                            </span>
+                        </span>
+                        <button>reap cuties for the price of {priceOfMint} ETH</button>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default Bubble;
